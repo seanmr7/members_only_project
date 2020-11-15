97738adceb625af17f692726c781bdd65bfe63d7
Sean <smriedl@gmail.com>
2020-11-14 19:42:09 -0500

    Initial commit. Setup devise and post scaffold.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,8 @@
+class RegistrationController < Devise::RegistrationController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
